53c24889f00a1cfa9ba4210a52893cc39001df61
Pablo Maribondo <pablomgvieira@gmail.com>
2019-10-08 23:38:58 -0300

    Helper para logar o usuário no site

---
+++ Helpers/Login.js
@@ -0,0 +1,25 @@
+const response = require('../response');
+const sessionId = require('../Helpers/SessionId');
+
+/**
+ * Função para executar o login do usuário e retornar um JSESSIONID válido
+ * @param {Object} request - Objeto contendo a matrícula, dígito e senha do usuário
+ * @param {Reply} reply - Resposta para o cliente
+ * @returns {string} Sufixo da url com um JSESSIONID válido
+ */
+async function get(request, reply) {
+  try {
+    const loginResponse = await sessionId.get(process.env.COLLEGE_LOGIN_URL, reply);
+
+    const url = `${process.env.COLLEGE_LOGIN_URL}${loginResponse}`;
+    const loggedInResponse = await sessionId.getWithRoutine(url, request, 1, reply);
+
+    return loggedInResponse;
+  } catch (error) {
+    return response.badRequest(error.message, '', reply);
+  }
+}
+
+module.exports = {
+  get,
+};
