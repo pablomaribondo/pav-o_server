44dd3a8ed30809bed38514add248412c98875327
Pablo Maribondo <pablomgvieira@gmail.com>
2019-12-10 13:55:51 -0300

    fix:Ajuste no endereço do server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ fastify.use(cors());
 
 const start = async () => {
   try {
-    await fastify.listen(process.env.PORT || 3000);
+    await fastify.listen(process.env.PORT || 3000, '0.0.0.0');
 
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (error) {
